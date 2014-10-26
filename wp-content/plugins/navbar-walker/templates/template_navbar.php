7a23532dc22a9475884b3f9860678c5f27decde9
Graeme Fulton <graemefulton@uk.ibm.com>
2014-10-26 21:35:21 +0000

    new theme

--- wp-content/plugins/navbar-walker/templates/template_navbar.php
+++ wp-content/plugins/navbar-walker/templates/template_navbar.php
@@ -2,8 +2,14 @@
 <nav class="navbar navbar-default" role="navigation">
   <div class="container menu-container">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header col-md-3">
-        <a class="navbar-brand" href="<?php bloginfo('url')?>"><img class="navbar-logo"src="<?php echo plugins_url('images/logo.png', __FILE__);?>"/></a>        
+    <div class="navbar-header col-md-12">
+       <center>
+
+        <a class="nb" href="<?php bloginfo('url')?>">
+            <img class="navbar-logo" src="<?php echo plugins_url('images/logo_1.png', __FILE__);?>"/>
+        </a>    
+        </center>
+
          <!--Mobile Navigation-->
        <div class="container-fluid" style="text-align:center">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
@@ -18,7 +24,7 @@
     </div>
   
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse col-md-8" id="navbar-collapse">
+    <div class="collapse navbar-collapse col-md-12" id="navbar-collapse">
                  <?php /* Primary navigation */
    wp_nav_menu( array(
         'menu'              => 'top_menu',
